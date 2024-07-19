8b70c5f069082915f2e4b59ff71358b7f26fa753
mellisasuryani <2332099223@webmail.uad.ac.id>
2024-07-19 14:04:39 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static('uploads')); // Middleware untuk menyajikan file statis
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
